f8c70e39dba004b89f0d52172feee3115a589508
William Galeano <wigame3@gmail.com>
2021-10-26 22:26:29 -0500

    1. Creación de modelo de empleado "Empleado.cs" 2. Inclusión de librerías para iconos 3. Inclusión de Librería DataTables

---
+++ src/Empleados/Models/EmpleadosViewModels/Empleado.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Empleados.Models.EmpleadosViewModels
+{    
+    public class Empleado
+    {
+     
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Tipo Documento")]
+        public string Tipo_documento { get; set; }
+
+        [Required]
+        [Display(Name = "# Documento")]
+        public string Documento { get; set; }
+
+        [Required]
+        [Display(Name = "Nombres")]
+        public string Nombres { get; set; }
+
+        [Required]
+        [Display(Name = "Apellidos")]
+        public string Apellidos { get; set; }
+
+        [Required]
+        [Display(Name = "Área")]
+        public string Area { get; set; }
+
+        [Required]
+        [Display(Name = "Sub Área")]
+        public string Sub_area { get; set; }
+        
+    }
+}
